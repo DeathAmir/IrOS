b74b4a5f0be897323d9ddb17007147355450bf89
DeathAmir <promoly1234@gmail.com>
2026-08-15 21:04:32 +0330

    Add PS2 polling input API

--- src/drivers/input.hpp
+++ src/drivers/input.hpp
@@ -9,6 +9,7 @@ bool key_pop(KeyEvent& e);
 struct MouseState { int x,y; int dx,dy; uint8_t buttons; bool changed; };
 void mouse_init(int width,int height);
 void mouse_irq();
+void poll();
 MouseState mouse_state();
 void mouse_ack_changed();
 }
